fb57cd2acbf0cfb09e8bacac5082cd312fa5ea87
diegomdomi <diegomdomi@gmail.com>
2023-03-23 11:54:42 +0100

    connect db

---
+++ scripts.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Users(idUsers INT NOT NULL AUTO_INCREMENT,
+				   first_name VARCHAR (50),
+				   job_desk VARCHAR (50),
+				   schedules DATE,
+				   contact VARCHAR (50),
+                   PRIMARY KEY (idUsers));
--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,24 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'sql2023',
+  database: 'hotelmiranda'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err:any, rows:any, fields:any) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
+
 app.use('/login',routerLogin)
 
 app.use("/bookings",
